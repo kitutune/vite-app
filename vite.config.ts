aa307374629e3a3b53278fb0dafa8467870a5d49
kitutune <p.kitutune@gmail.com>
2022-11-27 04:28:35 +0900

    1st commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import tsconfigPaths from "vite-tsconfig-paths"; //baseUrl を利用できるように
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // tsconfigPaths(), //baseUrl を利用できるように
+  ],
+});
